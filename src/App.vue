152ec1c16cc8bbbe515514136542147f867da4e1
Tevea Chhem <teveachhem@gmail.com>
2025-03-20 11:38:22 +0700

    Prepare the Vue App Layout with TailwindCSS

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
 <template>
-    <RouterView />
+    <nav
+        class="h-16 border-b bg-muted/40 flex justify-between items-center px-8"
+    >
+        <form class="w-full max-w-96">
+            <input class="bg-background" type="text" placeholder="search..." />
+        </form>
+        <div class="h-8 w-8 rounded-full bg-white"></div>
+    </nav>
 </template>
