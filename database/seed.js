4b4b0c982e1675948d083a2a04a5851eb89be7c7
Tevea Chhem <teveachhem@gmail.com>
2025-04-02 17:16:49 +0700

    Changes to Default Email Provider in Supabase

--- database/seed.js
+++ database/seed.js
@@ -1,3 +1,5 @@
+/* eslint-env node */
+
 import { fakerEN_US as faker } from "@faker-js/faker";
 import { createClient } from "@supabase/supabase-js";
 
@@ -5,6 +7,12 @@ const supabase = createClient(
   process.env.VITE_SUPABASE_URL,
   process.env.SERVICE_ROLE_KEY,
 );
+
+const testingUserEmail = process.env.TESTING_USER_EMAIL;
+if (!testingUserEmail) {
+  console.error("Have you forgot to add TESTING_USER_EMAIL to your .env file?");
+  process.exit();
+}
 
 const logErrorAndExit = (tableName, error) => {
   console.error(
@@ -15,6 +23,61 @@ const logErrorAndExit = (tableName, error) => {
 
 const logStep = (stepMessage) => {
   console.log(stepMessage);
+};
+
+const PrimaryTestUserExists = async () => {
+  logStep("Checking if primary test user exists...");
+  const { data, error } = await supabase
+    .from("profiles")
+    .select("id, username")
+    .eq("username", "testaccount1")
+    .single();
+
+  if (error) {
+    console.log("Primary test user not found. Will create one.");
+    return false;
+  }
+
+  logStep("Primary test user is found.");
+  return data?.id;
+};
+
+const createPrimaryTestUser = async () => {
+  logStep("Creating primary test user...");
+  const firstName = "Test";
+  const lastName = "Account";
+  const userName = "testaccount1";
+  const email = testingUserEmail;
+  const { data, error } = await supabase.auth.signUp({
+    email: email,
+    password: "12345678",
+    options: {
+      data: {
+        first_name: firstName,
+        last_name: lastName,
+        full_name: firstName + " " + lastName,
+        username: userName,
+      },
+    },
+  });
+
+  if (error) {
+    logErrorAndExit("Users", error);
+  }
+
+  if (data) {
+    const userId = data.user.id;
+    await supabase.from("profiles").insert({
+      id: userId,
+      full_name: firstName + " " + lastName,
+      username: userName,
+      bio: "The main testing account",
+      avatar_url: `https://i.pravatar.cc/150?u=${data.user.id}`,
+    });
+
+    logStep("Primary test user created successfully.");
+    return userId;
+  }
 };
 
 const seedProjects = async (numEntries, userId) => {
@@ -50,16 +113,14 @@ const seedTasks = async (numEntries, projectsIds, userId) => {
   const tasks = [];
 
   for (let i = 0; i < numEntries; i++) {
-    const name = faker.lorem.words(3);
-
     tasks.push({
-      name: name,
+      name: faker.lorem.words(3),
       status: faker.helpers.arrayElement(["in-progress", "completed"]),
       description: faker.lorem.paragraph(),
       due_date: faker.date.future(),
-      // profile_id: userId,
+      profile_id: userId,
       project_id: faker.helpers.arrayElement(projectsIds),
-      collaborators: faker.helpers.arrayElements([1, 2, 3]),
+      collaborators: faker.helpers.arrayElements([userId]),
     });
   }
 
@@ -76,12 +137,23 @@ const seedTasks = async (numEntries, projectsIds, userId) => {
 };
 
 const seedDatabase = async (numEntriesPerTable) => {
-  const projectsIds = (await seedProjects(numEntriesPerTable, 3, 2, 4)).map(
+  let userId;
+
+  const testUserId = await PrimaryTestUserExists();
+
+  if (!testUserId) {
+    const primaryTestUserId = await createPrimaryTestUser();
+    userId = primaryTestUserId;
+  } else {
+    userId = testUserId;
+  }
+
+  const projectsIds = (await seedProjects(numEntriesPerTable, userId)).map(
     (project) => project.id,
   );
-  await seedTasks(numEntriesPerTable, projectsIds);
+  await seedTasks(numEntriesPerTable, projectsIds, userId);
 };
 
 const numEntriesPerTable = 10;
 
-await seedDatabase(numEntriesPerTable);
+seedDatabase(numEntriesPerTable);
