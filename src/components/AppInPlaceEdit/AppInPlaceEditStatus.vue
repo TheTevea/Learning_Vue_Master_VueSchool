3e81cf614ef7cfba5ae564da8d68b0d0071c2872
Tevea Chhem <teveachhem@gmail.com>
2025-07-14 09:02:13 +0700

    Use Vue.js Props Destructure to Assign Default Values for Props

--- src/components/AppInPlaceEdit/AppInPlaceEditStatus.vue
+++ src/components/AppInPlaceEdit/AppInPlaceEditStatus.vue
@@ -1,24 +1,51 @@
 <script setup lang="ts">
 const value = defineModel();
+
+const emit = defineEmits(['commit']);
+
+const { readonly = false } = defineProps<{
+    readonly?: boolean;
+}>();
+
+const toggleStatus = () => {
+    if (readonly) return;
+    value.value = value.value === 'completed' ? 'in-progress' : 'completed';
+    emit('commit');
+};
 </script>
 
 <template>
-    <div>
-        <iconify-icon
-            v-if="value === 'completed'"
-            icon="lucide:circle-check"
-            class="text-green-500"
-            width="22"
-            height="22"
-        />
-        <iconify-icon
-            v-else
-            icon="lucide:circle-dot"
-            class="text-gray-500"
-            width="22"
-            height="22"
-        />
+    <div
+        :class="['text-2xl', !readonly && 'cursor-pointer']"
+        @click="toggleStatus"
+    >
+        <Transition mode="out-in">
+            <iconify-icon
+                v-if="value === 'completed'"
+                icon="lucide:circle-check"
+                class="text-green-500"
+                width="22"
+                height="22"
+            />
+            <iconify-icon
+                v-else
+                icon="lucide:circle-dot"
+                class="text-gray-500"
+                width="22"
+                height="22"
+            />
+        </Transition>
     </div>
 </template>
 
-<!--'w-full p-1 bg-transparent focus:outline-none focus:border-none focus:bg-gray-800 focus:rounded-md'-->
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+    transition: transform 0.1s;
+}
+
+.v-enter-from,
+.v-leave-to {
+    transform: scale(0.3);
+}
+</style>
